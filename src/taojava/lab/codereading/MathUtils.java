76d1a995bee7dd3f213b77d0717ec107b1d0b880
huann <huann@grinnell.edu>
2013-09-08 16:32:16 -0500

    reformat

--- src/taojava/lab/codereading/MathUtils.java
+++ src/taojava/lab/codereading/MathUtils.java
@@ -6,16 +6,14 @@ package taojava.lab.codereading;
  * @author Samuel A. Rebelsky
  */
 public class MathUtils {
+	
     /**
      * Square an integer.
      */
-    public static 
-    int square(int x) 
-    { return
-	    x * x;  } // square(int)
+    public static int square(int x) { return x * x; } // square(int)
     
     /** 
      * Square a double.
      */
-    public static double square(double x) { return x+x; } // square(double)
+    public static double square(double x) { return x * x; } // square(double)
 } // class MathUtils
